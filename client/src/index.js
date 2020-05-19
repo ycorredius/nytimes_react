a0f66258e82311c0fcd4d6c7874809429ee5f849
shaun <ycorredius@gmail.com>
2020-05-19 10:32:19 -0400

    "Fixing disconnect between back and front ends"

--- client/src/index.js
+++ client/src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {createStore} from 'redux'
+import {Provider} from 'redux-react'
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+const store = createStore(
+  shoppingListItemReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-    <BrowserRouter>
-      <App /> 
-    </BrowserRouter>,
+    <Provider>
+	    <BrowserRouter>
+	      <App /> 
+	    </BrowserRouter>,
+	</Provider>
   document.getElementById('root')
 );
 

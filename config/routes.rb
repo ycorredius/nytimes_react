db51ec17cda54bb24eba39b2ed8d5fe16485a9e3
shaun <ycorredius@gmail.com>
2020-05-23 08:16:34 -0400

    added routes api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 	resources :users, only: [:create, :show	]
-	post 'user_token' => 'user_token#create'
-	post 'find_user' => 'users#find'
-
+	post '/login', to: 'sessions#create'
+	delete '/logout', to: 'sessions#destroy'
+	get '/logged_in', to: 'sessions#is_logged_in?'
 end

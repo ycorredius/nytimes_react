66dfaeaa102513f1ed815d1e82ff0b960fe21635
shaun <ycorredius@gmail.com>
2020-06-05 12:38:03 -0400

    fixed serialized data

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: session_params[:email])
     if @user && @user.authenticate(session_params[:password])
+      options = {}
+      options[:include] = [:best_sellers]
       login!
       render json: {
         logged_in: true,
-        user: @user
+        user: UserSerializer.new(@user,options).serialized_json
       }
     else
       render json: { 
